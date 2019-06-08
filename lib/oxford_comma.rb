1100c8efb0a635d81705d75b7ed583ff2f3f6287
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-08 03:21:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    array.join(" and ") << ", and #{x}"
+    array.join(" and ")
   else
     x = array.pop
 
