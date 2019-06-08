698b5a13c807bab230462086826a2c9874b2cbd3
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-08 03:20:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
 
+  else
+    x = array.pop
+
+    array.join(", ") << ", and #{x}"
+  end
+  # last = array.pop
+  #
+  # array.join(", ")
+  # #array.push("and ")
+  # array.push("and #{last}")
+  #
+  # return array
 end
